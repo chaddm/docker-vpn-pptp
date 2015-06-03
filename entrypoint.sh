e64d438ec93692905821ff7c33ec40bbd0f6a3ef
Tony.Shao <xiocode@gmail.com>
2015-06-03 14:56:47 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,10 @@ service rsyslog start
 # enable IP forwarding
 sysctl -w net.ipv4.ip_forward=1
 
+sysctl -p
+
 # configure firewall
-iptables -t nat -A POSTROUTING -s 10.99.99.0/24 -o eth0 -j MASQUERADE
-iptables -A FORWARD -s 10.99.99.0/24 -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -j TCPMSS --set-mss 1356
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE && iptables-save
+#iptables -A FORWARD -s 10.99.99.0/24 -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -j TCPMSS --set-mss 1356
 
 exec "$@"
