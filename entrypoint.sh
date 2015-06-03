e22e1fbd10559fe666cff60a18b10e970d2f5351
Tony.Shao <xiocode@gmail.com>
2015-06-03 18:41:20 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,16 +8,9 @@ service rsyslog restart
 sysctl -w net.ipv4.ip_forward=1
 sysctl -p
 
-
-#control channel
-iptables -A INPUT -p tcp --dport 1723 -j ACCEPT
-
-# Accept GRE packets
-iptables -A INPUT -p 47 -j ACCEPT
-iptables -A OUTPUT -p 47 -j ACCEPT
-
-iptables -t nat -A POSTROUTING -s 10.99.99.0/24 -o eth0 -j MASQUERADE
-#supposedly makes the vpn work better
-iptables -I FORWARD -s 10.99.99.0/24 -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -j TCPMSS --set-mss 1356
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables --table nat --append POSTROUTING --out-interface ppp0 -j MASQUERADE
+iptables -I INPUT -s 10.99.99.0/24 -i ppp0 -j ACCEPT
+iptables --append FORWARD --in-interface eth0 -j ACCEPT
 
 exec "$@"
