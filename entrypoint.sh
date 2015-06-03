df24f60e3bd8c1ecd9b291246dd6c25bd64017b6
Tony.Shao <xiocode@gmail.com>
2015-06-03 15:51:33 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 sysctl -p
 
 # configure firewall
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE && iptables-save
-#iptables -A FORWARD -s 10.99.99.0/24 -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -j TCPMSS --set-mss 1356
+iptables -t nat -A POSTROUTING -s 10.99.99.0/24 -o eth0 -j MASQUERADE
+iptables -A FORWARD -s 10.99.99.0/24 -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -j TCPMSS --set-mss 1356
 
 exec "$@"
