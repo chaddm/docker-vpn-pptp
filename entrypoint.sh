705063e8777c6cab4ec8ed94e0c5ed4c0c4534e9
Tony.Shao <xiocode@gmail.com>
2015-09-16 17:37:39 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,12 @@ sysctl -p
 
 echo "localip 10.99.99.1" >> /etc/pptpd.conf
 echo "remoteip 10.99.99.100-199" >> /etc/pptpd.conf
+echo "debug" >> /etc/pptpd.conf
 echo "tian pptpd 299792458 *" >> /etc/ppp/chap-secrets
 echo "ms-dns 8.8.8.8" >> /etc/ppp/pptpd-options
 echo "ms-dns 8.8.4.4" >> /etc/ppp/pptpd-options
+echo "debug" >> /etc/ppp/pptpd-options
+echo "dump" >> /etc/ppp/pptpd-options
 
 iptables -A INPUT -p tcp --dport 1723 -j ACCEPT
 iptables -A INPUT -p gre -j ACCEPT
