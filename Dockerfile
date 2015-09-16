3ba7007d44f12e14349ce529cd58940a93a712ca
Tony.Shao <xiocode@gmail.com>
2015-09-16 16:33:03 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM ubuntu:latest
 MAINTAINER Tony.Shao <xiocode@gmail.com>
 
 ENV DEBIAN_FRONTEND noninteractive
-RUN apt-get update -qq && apt-get install -qqy pptpd iptables && apt-get clean
+RUN apt-get update -qq && apt-get install -qqy pptpd iptables && apt-get clean && rm -rf /var/lib/apt/lists/*
 
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod 0700 /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 sysctl -w net.ipv4.ip_forward=1
 sysctl -p
 
-echo "localip 0.0.0.0" >> /etc/pptpd.conf
+echo "localip 10.99.99.1" >> /etc/pptpd.conf
 echo "remoteip 10.99.99.100-199" >> /etc/pptpd.conf
 echo "tian pptpd 299792458 *" >> /etc/ppp/chap-secrets
 echo "ms-dns 8.8.8.8" >> /etc/ppp/pptpd-options
@@ -14,5 +14,6 @@ iptables -A INPUT -p tcp --dport 1723 -j ACCEPT
 iptables -A INPUT -p gre -j ACCEPT
 iptables -t nat -A POSTROUTING -s 10.99.99.0/24 -o eth0 -j MASQUERADE
 iptables -A FORWARD -p tcp --syn -s  10.99.99.0/24 -j TCPMSS --set-mss 1356
+
 
 pptpd --fg
