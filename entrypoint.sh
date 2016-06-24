8c5844b0263bf8ce9a87a7694c1dd3c979f8adbc
Chad Dumler-Montplaisir <chad.dumler.montplaisir@gmail.com>
2016-06-24 17:38:50 -0400

    Changed for Digital Ocean compatibility.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ service rsyslog restart
 sysctl -w net.ipv4.ip_forward=1
 sysctl -p
 
-echo "localip 10.99.99.1" >> /etc/pptpd.conf
-echo "remoteip 10.99.99.100-199" >> /etc/pptpd.conf
+echo "localip 10.0.0.1" >> /etc/pptpd.conf
+echo "remoteip 10.0.0.100-200" >> /etc/pptpd.conf
 echo "debug" >> /etc/pptpd.conf
 echo "tian pptpd 299792458 *" >> /etc/ppp/chap-secrets
 echo "ms-dns 8.8.8.8" >> /etc/ppp/pptpd-options
@@ -17,8 +17,8 @@ echo "dump" >> /etc/ppp/pptpd-options
 
 iptables -A INPUT -p tcp --dport 1723 -j ACCEPT
 iptables -A INPUT -p gre -j ACCEPT
-iptables -t nat -A POSTROUTING -s 10.99.99.0/24 -o eth0 -j MASQUERADE
-iptables -A FORWARD -p tcp --syn -s  10.99.99.0/24 -j TCPMSS --set-mss 1356
+iptables -t nat -A POSTROUTING -s 10.0.0.0/24 -o eth0 -j MASQUERADE
+iptables -A FORWARD -p tcp --syn -s  10.0.0.0/24 -j TCPMSS --set-mss 1356
 
 
 pptpd --fg
